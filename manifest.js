d195e9c60302f4d0876e23a162f16e91741e281b
soophajuka-design <soophajuka@gmail.com>
2026-03-28 00:25:19 +0700

    Delete manifest.js

--- manifest.js
+++
@@ -1,9 +0,0 @@
-{
-  "name": "G6PD Analyzer",
-  "short_name": "G6PD",
-  "start_url": "./index.html",
-  "display": "standalone",
-  "background_color": "#000000",
-  "theme_color": "#000000",
-  "icons": []
-}
